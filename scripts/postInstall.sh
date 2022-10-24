7307faacbddcfb36a226a20faad451ffe0c8b848
amsellemJoseph <you@example.com>
2022-10-24 09:44:00 +0300

    fix

--- scripts/postInstall.sh
+++ scripts/postInstall.sh
@@ -3,7 +3,7 @@ set -o allexport; source .env; set +o allexport;
 
 #wait until the server is ready
 echo "Waiting for software to be ready ..."
-sleep 10s;
+sleep 40s;
 
 #register the local server in the web ui
 target=$(docker-compose port traduora 8080)
--- scripts/preInstall.sh
+++ scripts/preInstall.sh
@@ -3,6 +3,3 @@ set -o allexport; source .env; set +o allexport;
 
 mkdir -p ./data
 chown -R 1000:1000 ./data
-
-echo "Waiting for software to be ready ..."
-sleep 10s;
