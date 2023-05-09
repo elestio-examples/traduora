2b5fce4b89d1cd69c1f8551bd8dd329720ce2984
AmsellemJoseph <amsellem.joseph@gmail.com>
2023-05-09 13:10:08 +0300

    tests

--- tests/build.sh
+++ tests/build.sh
@@ -1,3 +1,3 @@
-sed -i "s~COPY --from=builder /wait /docker-entrypoint.sh /docker-entrypoint.compose.sh ./~COPY --from=builder /wait /docker-entrypoint.sh /docker-entrypoint.compose.sh /README.md ./~g" ./Dockerfile
+sed -i "s~COPY --from=builder /wait /docker-entrypoint.sh /docker-entrypoint.compose.sh ./~COPY --from=builder /wait /docker-entrypoint.sh /docker-entrypoint.compose.sh ./\n\nCOPY README.md ./~g" ./Dockerfile
 
 docker build . --tag elestio4test/traduora:latest
